dfe5e02496c57391069ee06e35070c728e89b526
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-06-11 09:42:41 +0000

    途中

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' =>'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show',as: 'book'
+  get 'books/:id/edit'=> 'lists#edit',as: 'edit_list'
   get 'top' =>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
