5308114bb817b343b8932b898678c17573977767
naaaaaaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-06-12 07:32:25 +0000

    バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
   
   def create
-   book = Book.new(book_params)
-   book.save
-   redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -30,7 +32,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to'/book'
+    redirect_to'/books'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   post 'books' =>'books#create'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show',as: 'book'
-  get 'books/:id/edit'=> 'books#edit',as: 'edit_list'
+  get 'books/:id/edit'=> 'books#edit',as: 'edit_book'
   patch 'books/:id' => 'books#update',as: 'update_book'
   delete 'books/:id' => 'books#destroy',as: 'destroy_book'
+
   get 'top' =>'homes#top'
   
   
